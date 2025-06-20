6238d5af341a44136802d45a67a32bc63be07e80
KiaraLuz <fernandezperezkiara@gmail.com>
2025-06-20 09:45:49 -0500

    feat: :tada: Inicio del proyecto
    Se inició el proyecto con Vite y Tailwindcss, se instaló Shadcn con los ui button, dialog, input, skeleton, sonner y table. También pinia para el almacenamiento y vee validate.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Toaster } from "@/components/ui/sonner";
+import "vue-sonner/style.css";
+</script>
+
+<template>
+  <div class="font-sans">
+    <Toaster />
+    <h1 class="text-3xl">Prueba técnica</h1>
+  </div>
+</template>
